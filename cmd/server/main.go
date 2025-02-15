29ac1a11a0e695ba8c23ab48c9b81638f4df0bd5
Pablo <yariongranham@gmail.com>
2025-02-15 16:05:19 -0300

    add midddleware auth and verify token of user

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"pos-go-api/configs"
@@ -12,7 +14,7 @@ import (
 )
 
 func main() {
-	_, err := configs.LoadConfig(".")
+	config, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
@@ -23,17 +25,35 @@ func main() {
 	}
 
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
 	ProductDB := database.NewProduct(db)
 	ProductHandler := handlers.NewProductHandler(ProductDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB, config.JWTSecret, config.JWTExpiresIn)
+
 	f := fiber.New()
 	f.Use(logger.New())
+	f.Use(recover.New())
 
-	f.Post("/products", ProductHandler.CreateProduct)
-	f.Get("/products/:id", ProductHandler.GetProduct)
-	f.Get("/products", ProductHandler.GetProducts)
-	f.Put("/products/:id", ProductHandler.UpdateProduct)
-	f.Delete("/products/:id", ProductHandler.DeleteProduct)
+	jwtMiddleware := jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{
+			Key: []byte(config.JWTSecret),
+		},
+		ContextKey: "user",
+	})
+
+	f.Route("/products", func(router fiber.Router) {
+		router.Use(jwtMiddleware)
+		router.Post("/", ProductHandler.CreateProduct)
+		router.Get("/:id", ProductHandler.GetProduct)
+		router.Get("/", ProductHandler.GetProducts)
+		router.Put("/:id", ProductHandler.UpdateProduct)
+		router.Delete("/:id", ProductHandler.DeleteProduct)
+	})
+
+	f.Post("/users", userHandler.Create)
+	f.Post("/users/generate_token", userHandler.GetJWT)
 
 	err = f.Listen(":8000")
 	if err != nil {
