2e3344f36619d45c18542ad54c46154cb03106ab
Pablo <yariongranham@gmail.com>
2025-02-14 23:51:53 -0300

    create base server and handle product for test create product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+	"pos-go-api/configs"
+	"pos-go-api/internal/dto"
+	"pos-go-api/internal/entity"
+	"pos-go-api/internal/infra/database"
+)
 
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	ProductDB := database.NewProduct(db)
+	ProductHandler := NewProductHandler(ProductDB)
+
+	http.HandleFunc("/products", ProductHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	productDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		productDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.productDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
